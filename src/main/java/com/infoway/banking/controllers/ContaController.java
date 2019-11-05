a13ce7ef3b9a31a82c33dff20bd649c31befedb7
Raffael404 <raffael404@gmail.com>
2019-11-05 02:57:39 -0300

    Implementação dos métodos sacar e transferir em ContaController.

--- src/main/java/com/infoway/banking/controllers/ContaController.java
+++ src/main/java/com/infoway/banking/controllers/ContaController.java
@@ -117,7 +117,6 @@ public class ContaController {
 			conta = contaService.buscar(banco.get(), transacaoDto.getContaDestino());
 			if (!conta.isPresent())
 				result.addError(new ObjectError("conta", "Conta de destino inexistente."));
-			
 		}
 		
 		Response<TransacaoDto> response = new Response<TransacaoDto>();
@@ -135,7 +134,91 @@ public class ContaController {
 		
 		conta.get().depositar(transacaoDto.getValor());
 		contaService.persistir(conta.get());
-		System.out.println(conta.get().getSaldo());
+		
+		response.setData(transacaoDto);
+		return ResponseEntity.ok(response);
+	}
+	
+	@PostMapping(value = "/sacar")
+	public ResponseEntity<Response<TransacaoDto>> sacar(@Valid @RequestBody TransacaoDto transacaoDto, BindingResult result) {
+		log.info("Sacando R$ {} na conta {} no banco {}", transacaoDto.getValor(),
+				transacaoDto.getContaOrigem(), transacaoDto.getBancoOrigem());
+				
+		Optional<Banco> banco = bancoService.buscar(transacaoDto.getBancoOrigem());
+		Optional<Conta> conta = null;
+		if (!banco.isPresent())
+			result.addError(new ObjectError("banco", "Banco de origem inexistente."));
+		else {
+			conta = contaService.buscar(banco.get(), transacaoDto.getContaOrigem());
+			if (!conta.isPresent())
+				result.addError(new ObjectError("conta", "Conta de origem inexistente."));
+		}
+		
+		Response<TransacaoDto> response = new Response<TransacaoDto>();
+		if (result.hasErrors()) {
+			log.error("Erro validando dados da conta de origem: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		Transacao transacao = new Transacao();
+		transacao.setOrigem(conta.get());
+		transacao.setTipo(TipoTransacao.SAQUE);
+		transacao.setValor(transacaoDto.getValor());
+		transacaoService.persistir(transacao);
+		
+		conta.get().sacar(transacaoDto.getValor());
+		contaService.persistir(conta.get());
+		
+		response.setData(transacaoDto);
+		return ResponseEntity.ok(response);
+	}
+	
+	@PostMapping(value = "/transferir")
+	public ResponseEntity<Response<TransacaoDto>> transferir(@Valid @RequestBody TransacaoDto transacaoDto, BindingResult result) {
+		log.info("Transferindo R$ {} da conta {} no banco {} para a conta {} no banco {}", transacaoDto.getValor(),
+				transacaoDto.getContaOrigem(), transacaoDto.getBancoOrigem(),
+				transacaoDto.getContaDestino(), transacaoDto.getBancoDestino());
+				
+		Optional<Banco> bancoOrigem = bancoService.buscar(transacaoDto.getBancoOrigem());
+		Optional<Conta> contaOrigem = null;
+		if (!bancoOrigem.isPresent())
+			result.addError(new ObjectError("banco", "Banco de origem inexistente."));
+		else {
+			contaOrigem = contaService.buscar(bancoOrigem.get(), transacaoDto.getContaOrigem());
+			if (!contaOrigem.isPresent())
+				result.addError(new ObjectError("conta", "Conta de origem inexistente."));
+		}
+		
+		Optional<Banco> bancoDestino = bancoService.buscar(transacaoDto.getBancoDestino());
+		Optional<Conta> contaDestino = null;
+		if (!bancoDestino.isPresent())
+			result.addError(new ObjectError("banco", "Banco de destino inexistente."));
+		else {
+			contaDestino = contaService.buscar(bancoDestino.get(), transacaoDto.getContaDestino());
+			if (!contaDestino.isPresent())
+				result.addError(new ObjectError("conta", "Conta de destino inexistente."));
+		}
+		
+		Response<TransacaoDto> response = new Response<TransacaoDto>();
+		if (result.hasErrors()) {
+			log.error("Erro validando dados das contas: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		Transacao transacao = new Transacao();
+		transacao.setOrigem(contaOrigem.get());
+		transacao.setDestino(contaDestino.get());
+		transacao.setTipo(TipoTransacao.TRANSFERENCIA);
+		transacao.setValor(transacaoDto.getValor());
+		transacaoService.persistir(transacao);
+		System.out.println(transacao);
+		
+		contaOrigem.get().sacar(transacaoDto.getValor());
+		contaDestino.get().depositar(transacaoDto.getValor());
+		contaService.persistir(contaOrigem.get());
+		contaService.persistir(contaDestino.get());
 		
 		response.setData(transacaoDto);
 		return ResponseEntity.ok(response);
