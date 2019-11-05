382a897b7afc33100e6c2c8dbec087fcd10dcee7
Raffael404 <raffael404@gmail.com>
2019-11-05 15:31:03 -0300

    Implementação da entidade Agencia e de seu respectivo DTO.

---
+++ src/main/java/com/infoway/banking/dtos/AgenciaDto.java
@@ -0,0 +1,53 @@
+package com.infoway.banking.dtos;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import com.infoway.banking.entities.Agencia;
+
+public class AgenciaDto {
+	
+	private String cnpj;
+	private String numero;
+	private String codigoBanco;
+	
+	public AgenciaDto() {}
+	
+	public AgenciaDto(Agencia agencia) {
+		this.cnpj = agencia.getCnpj();
+		this.numero = agencia.getNumero();
+		this.codigoBanco = agencia.getBanco().getCodigo();
+	}
+
+	@NotEmpty(message = "CNPJ não pode ser vazio.")
+	@Length(min = 14, max = 14, message = "CNPJ deve conter 14 caracteres.")
+	@CNPJ(message = "CNPJ inválido.")
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	@NotEmpty(message = "Número não pode ser vazio.")
+	@Length(min = 1, max = 5, message = "Número deve conter entre 1 e 5 caracteres.")
+	@Pattern(regexp = "[\\d]*", message = "Número deve conter apenas números.")
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	@NotEmpty(message = "Código do banco não pode ser vazio.")
+	public String getCodigoBanco() {
+		return codigoBanco;
+	}
+	public void setCodigoBanco(String codigoBanco) {
+		this.codigoBanco = codigoBanco;
+	}
+	
+}
---
+++ src/main/java/com/infoway/banking/entities/Agencia.java
@@ -0,0 +1,47 @@
+package com.infoway.banking.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "agencia")
+public class Agencia implements Serializable {
+
+	private static final long serialVersionUID = 8363672344428972513L;
+	
+	private String cnpj;
+	private String numero;
+	private Banco banco;
+	
+	@Id
+	@Column(name = "cnpj", nullable = false, length = 14)
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	@Column(name = "numero", nullable = false, length = 5)
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	public Banco getBanco() {
+		return banco;
+	}
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+	
+}
